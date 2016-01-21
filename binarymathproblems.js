4b94fc45424c064403747d441f105cd6241805b1
Will Wheeler <willfulbard@gmail.com>
2016-01-21 15:29:04 -0800

    Add some styling to the page

--- binarymathproblems.js
+++ binarymathproblems.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     var numberMax = Math.pow(2, places + 1);
     var number1 = Math.floor(Math.random() * numberMax);
     var number2 = Math.floor(Math.random() * numberMax);
+    var longestNumber = Math.max(number1.toString(2).length, number2.toString(2).length);
     //Make it easy so that we dont get negatives or fractions
     if ((operator === '-') && number1 < number2) {
       var tmp = number1;
@@ -14,11 +15,11 @@ $(document).ready(function() {
     }
 
     var html = '<div class="problem" data-number1="'+number1+'" data-number2="'+number2+'" data-operator="'+operator+'">';
-    html +=      '<div class="math">';
+    html +=      '<pre class="math">';
     html +=       number1.toString(2)+'<br />';
-    html +=       operator+' '+number2.toString(2)+'<br />';
-    html +=       Array(places+2).join('-');
-    html +=      '</div>';
+    html +=       operator;
+    html +=       String(Array(places+3).join(" ")+number2.toString(2)).slice((longestNumber+1)*-1)+'<br />';
+    html +=      '</pre>';
     html +=      '<input class="answer" />';
     html +=    '</div>';
     $('.problems').append(html);
